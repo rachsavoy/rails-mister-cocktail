36fbb19e303a8e9b97b8ab8f4e6bf101ab129829
Rachael Savoy <rachael.a.hakim@gmail.com>
2020-11-19 21:13:40 +0900

    added style to doses new

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,36 @@
 class DosesController < ApplicationController
+
+  def index
+    @doses = Dose.all
+  end
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
 end
