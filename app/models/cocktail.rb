5ec44d889c47a03ab697329dc2ef7fff03ca050e
Rachael Savoy <rachael.a.hakim@gmail.com>
2020-11-19 16:12:59 +0900

    models completed

---
+++ app/models/cocktail.rb
@@ -0,0 +1,10 @@
+class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+
+  validates :name, presence: true, uniqueness: true
+
+  def index
+
+  end
+end
